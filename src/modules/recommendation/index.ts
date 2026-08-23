38cf459b8ccf0f129300b35185766c94506ddf1f
ptutorial <ptutorial3@gmail.com>
2026-08-23 06:14:09 +0530

    refactor: add modular recommendation boundary

---
+++ src/modules/recommendation/index.ts
@@ -0,0 +1,4 @@
+export * from '../../recommendation.js';
+export * from '../../recommendation-orchestrator.js';
+export * from '../../recommendation-schema.js';
+export * from '../../recommendation-scoring.js';
