a712409e99fa945d5d2b678c9089856157fe9b28
ptutorial <ptutorial3@gmail.com>
2026-08-23 06:14:14 +0530

    refactor: add modular market data boundary

---
+++ src/modules/market/index.ts
@@ -0,0 +1,5 @@
+export * from '../../market-data.js';
+export * from '../../stock-snapshot.js';
+export * from '../../data-sources.js';
+export * from '../../local-db-repository.js';
+export { YahooFinanceDataSource } from '../../providers/yahoo-finance.js';
